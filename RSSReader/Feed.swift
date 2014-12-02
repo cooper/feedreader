97db562054ca00b61775735c8058739f42e8e80b
Mitchell Cooper <githubspam@rlygd.net>
2014-12-01 19:23:51 -0500

    move XML parsing out of Feed and into XMLParser

--- RSSReader/Feed.swift
+++ RSSReader/Feed.swift
@@ -45,10 +45,6 @@ class Feed: NSManagedObject, Printable, ArticleCollection, NSXMLParserDelegate {
     //
     @NSManaged var managedArticles: NSOrderedSet
     
-    lazy var mutableArticles: NSMutableOrderedSet = {
-        return self.mutableOrderedSetValueForKey("managedArticles")
-    }()
-    
     var articles: [Article] {
         return self.managedArticles.array as [Article]
     }
@@ -61,9 +57,13 @@ class Feed: NSManagedObject, Printable, ArticleCollection, NSXMLParserDelegate {
         return byId
     }
     
+    lazy var mutableArticles: NSMutableOrderedSet = {
+        return self.mutableOrderedSetValueForKey("managedArticles")
+    }()
+    
     // Groups
     //
-    // this property is not necessarily used, but only represents the
+    // this property is not necessarily used, but it represents the
     // inverse of the FeedGroup.feeds relationship.
     //
     
@@ -118,11 +118,11 @@ class Feed: NSManagedObject, Printable, ArticleCollection, NSXMLParserDelegate {
         }
         return defaultImage
     }()
-    
+
     // MARK:- Non-persistent state properties
     
-    private var shouldFetchIcon = false     // whether it's necessary to fetch icon
-    private var shouldFetchLogo = false     // whether it's necessary to fetch logo
+    var shouldFetchIcon = false     // whether it's necessary to fetch icon
+    var shouldFetchLogo = false     // whether it's necessary to fetch logo
     
     var loading = false                     // is it being fetched now?
     weak var currentGroup: FeedGroup?       // current feed group in user interface
@@ -177,12 +177,9 @@ class Feed: NSManagedObject, Printable, ArticleCollection, NSXMLParserDelegate {
                 return
             }
             
-            // FIXME: is it possible for this to fail?
-            let parser = NSXMLParser(data: data)
-            parser.delegate = self
-            
             // initiate XML parser in this same queue.
             //NSLog("Parsing the data")
+            let parser = XMLParser(feed: self, data: data)
             parser.parse()
             rss.currentFeedVC?.tableView.reloadData()
 
@@ -236,258 +233,16 @@ class Feed: NSManagedObject, Printable, ArticleCollection, NSXMLParserDelegate {
         fetchImage(logoUrlString, shouldFetchLogo) {
             data, image in
             self.logoData = data
-            self.logo = image
         }
         fetchImage(iconUrlString, shouldFetchIcon) {
             data, image in
             self.iconData = data
-            self.icon = image
         }
     }
     
     // convenience for fetching with no callback.
     func fetch() {
         fetchThen(nil)
-    }
-
-    // MARK:- XML parser state
-    
-    // this class represents a single specific XML element.
-    // its purpose is to create a hierarchy of elements.
-    class Element {
-        
-        // these are the element types recognized by this RSS/Atom parser.
-        enum Kind {
-            
-                                // Description                  RSS                 Atom
-                                // --------------               ------              -------
-
-            case None           // No element (main scope)      n/a                 n/a
-            case Unknown        // Unknown element type         n/a                 n/a
-            
-            case FeedTitle      // title of the feed            <title>             <title>
-            case FeedImage      // image for feed               <image>             n/a
-            case FeedIconURL    // feed image URL               <url>               <icon>
-            case FeedLogoURL    // feed image URL               <url>               <logo>
-            case Channel        // RSS feed channel             <channel>           <feed>
-            case Link           // URL for feed                 <link>              <link>
-            
-            case Item           // an item or article           <item>              <entry>
-            case ItemTitle      // title of an item             <title>             <title>
-            case ItemId         // identifier of item           <guid>              <id>
-            case ItemDesc       // description of item          <description>       <summary>
-            case ItemPubDate    // date of item publish         <pubDate>           <published>
-            
-        }
-        
-        // type of the element and its parent, if any.
-        var type = Kind.None
-        var parent: Element?
-        
-        // initialize with a type.
-        convenience init(kind: Kind) {
-            self.init()
-            type = kind
-        }
-        
-    }
-    
-    // current article and element while parsing.
-    var article : Article?
-    var element = Element()
-    
-    // open an element as a child of the current element.
-    func adoptNewElement(type: Element.Kind) {
-        let old = element
-        element = Element(kind: type)
-        element.parent = old
-    }
-    
-    // close the current element.
-    func closeElement() {
-        element = element.parent ?? Element()
-    }
-    
-    // convenience for assigning/fetching.
-    var elementType: Element.Kind {
-        set { adoptNewElement(newValue) }
-        get { return element.type       }
-    }
-    
-    struct current {
-        static var itemPublishedDate = ""
-    }
-    
-    // MARK:- XML parsing
-    
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
-        let attributes = attributeDict as [String: String]
-        //NSLog("Starting element: \(elementName)")
-        
-        switch elementName {
-            
-            case "channel", "feed":
-                elementType = .Channel
-            
-            // if there's an article, this is its title.
-            case "title" where element.type == .Item && article != nil:
-                elementType = .ItemTitle
-                article?.title = ""
-            
-            // otherwise, this is the feed title.
-            case "title" where element.type == .Channel:
-                elementType = .FeedTitle
-            
-            // the start of an article.
-            case "item", "entry":
-                article = rss.manager.newArticleForFeed(self)
-                elementType = .Item
-
-            // link of an article.
-            case "link" where element.type == .Item && article != nil:
-                elementType = .Link
-
-                switch attributes["rel"] ?? "alternate" {
-                    
-                    // edit link.
-                    case "edit":
-                        NSLog("Edit")
-                    
-                    // the main link.
-                    case "alternate" where attributes["href"] != nil:
-                        article!.urlString = attributes["href"]!
-                    
-                    // some other.
-                    default:
-                        break
-                    
-                }
-            
-            // image of an article (RSS).
-            case "image" where element.type == .Channel:
-                elementType = .FeedImage
-            
-            // feed logo URL (RSS = url, Atom = logo).
-            case "url" where element.type == .FeedImage,
-                "logo" where element.type == .Channel:
-                
-                elementType = .FeedLogoURL
-            
-            // feed icon URL (Atom only).
-            case "icon" where element.type == .Channel:
-                
-                elementType = .FeedIconURL
-            
-            // identifier for an article or item.
-            case "guid" where element.type == .Item,
-                 "id"   where element.type == .Item:
-                
-                elementType = .ItemId
-            
-            // item description.
-            case "description" where element.type == .Item,
-                 "summary"     where element.type == .Item:
-                
-                elementType = .ItemDesc
-                article?.rawSummary = ""
-            
-            // item publish date.
-            case "pubDate"   where element.type == .Item,
-                 "published" where element.type == .Item:
-                
-                elementType = .ItemPubDate
-                current.itemPublishedDate = ""
-            
-            // some other element that we do not handle.
-            default:
-                //NSLog("Unkown element \(elementName)")
-                elementType = .Unknown
-            
-        }
-
-    }
-    
-    // found some characters that are not part of an element.
-    // note: it seems that this will be called several times with the data in pieces
-    // if the any of the characters are encoded with entities like &gt; etc.
-    func parser(parser: NSXMLParser, foundCharacters string: String) {
-        switch element.type {
-            
-            // the title of the feed.
-            case .FeedTitle:
-                channelTitle = string
-            
-            // the title of an article.
-            case .ItemTitle:
-                article?.title += string
-                //NSLog("adding to title: \(string)")
-            
-            // in RSS, the link is within <link> tags (handled here).
-            // in Atom, the link is in the href attribute.
-            case .Link:
-                article?.urlString = string
-
-            // icon for the feed.
-            case .FeedIconURL:
-                shouldFetchIcon = icon == defaultImage || string != iconUrlString
-                iconUrlString   = string
-            
-            // logo for the feed.
-            case .FeedLogoURL:
-                shouldFetchLogo = logo == defaultImage || string != logoUrlString
-                logoUrlString   = string
-            
-            // item identifier.
-            case .ItemId:
-                article?._identifier = string
-            
-            // item description.
-            case .ItemDesc:
-
-                if article?.rawSummary == nil {
-                    article?.rawSummary = string
-                }
-                else {
-                    article?.rawSummary! += string
-                }
-            
-            case .ItemPubDate:
-                current.itemPublishedDate += string
-            
-            // some other element...
-            default:
-                
-                break
-            
-        }
-    }
-
-    //func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String, qualifiedName qName: String) {
-    func parser(parser: NSXMLParser, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
-        switch element.type {
-            
-            case .Channel:
-                iconUrlString = iconUrlString ?? logoUrlString
-            
-            // add the current article to the feed.
-            // addArticle() MUST be called after the identifier (if any) has been determined.
-            case .Item:
-                addArticle(article!)
-                article = nil
-            
-            case .ItemPubDate:
-                article!.publishDate =
-                    NSDate.fromInternetString(current.itemPublishedDate) ??
-                    article!.publishDate
-            
-            case .ItemDesc:
-                article!.summary = article!.rawSummary?.withoutHTMLTagsAndNewlines
-            
-            default:
-                break
-            
-        }
-        closeElement()
     }
     
 }
